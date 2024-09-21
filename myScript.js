076b045ee8acc0c1b7131d7f3d18055dd86a2bf7
Whereisade <eniolaadeleke2303@gmail.com>
2024-09-21 14:42:07 +0100

    pushing

--- myScript.js
+++ myScript.js
@@ -1,2 +1,11 @@
-const dropDown = document.querySelector('.dropD');
-const 
+let menuList = document.querySelector('navList')
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px"){
+        menuList.style.maxHeight = "300px"
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
